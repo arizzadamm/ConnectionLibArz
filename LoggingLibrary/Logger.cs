4a82b106d0df46781d64f6247229c16027a52a1c
Ariza <arizadamar@live.com>
2024-07-04 11:23:30 +0700

    Add Logging Lib

---
+++ LoggingLibrary/Logger.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LoggingLibrary
+{
+    public static class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static Logger()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config");
+            XmlConfigurator.Configure(logRepository, new FileInfo(configFilePath));
+        }
+
+        public static void Debug(string message)
+        {
+            log.Debug(message);
+        }
+
+        public static void Info(string message)
+        {
+            log.Info(message);
+        }
+
+        public static void Warn(string message)
+        {
+            log.Warn(message);
+        }
+
+        public static void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public static void Fatal(string message)
+        {
+            log.Fatal(message);
+        }
+    }
+}
